e89288addd029fccb6a11ad4ddd0a8d7407dd596
squixone <lizard944@gmail.com>
2016-06-27 16:53:37 +0300

    1

---
+++ projects/barbershop/js/main.js
@@ -0,0 +1,64 @@
+var $linkLogin = document.querySelector(".login");
+var $popup = document.querySelector(".modal-content");
+var $overlay = document.querySelector(".modal-overlay");
+var $closePopup = document.querySelector(".modal-content-close");
+//Map
+var $linkMap = document.querySelector(".map");
+var $linkMap2 = document.querySelector(".map2");
+var $map = document.querySelector(".modal-content-map");
+var $closeMap = document.querySelector(".modal-content-close-map");
+var $closeMap2 = document.querySelector(".modal-content-close-map");
+
+//Popup open
+$linkLogin.addEventListener("click", function(event){ //отслеживает события клика
+  event.preventDefault();                             //ссылка не будет работать <a href=''> /#
+  $popup.classList.add("modal-content-show");         // добовляем клас css к тегу с класом .modal-content($), без точки
+  $overlay.classList.add("modal-overlay-show");       // добовляем клас css к тегу с класом .modal-overlay($), без точки
+});
+//Popup close
+$closePopup.addEventListener("click", function(event){
+  event.preventDefault();
+  $popup.classList.remove("modal-content-show");      //удаляет класс с тега
+  $overlay.classList.remove("modal-overlay-show");    //удаляет класс с тега
+});
+
+//Map open
+$linkMap.addEventListener("click", function(event){
+  event.preventDefault();
+  $map.classList.add("modal-content-map-show");
+  $overlay.classList.add("modal-overlay-show");
+})
+$linkMap2.addEventListener("click", function(event){
+  event.preventDefault();
+  $map.classList.add("modal-content-map-show");
+  $overlay.classList.add("modal-overlay-show");
+})
+//Map close
+$closeMap.addEventListener("click", function(event){
+  event.preventDefault();
+  $map.classList.remove("modal-content-map-show");
+  $overlay.classList.remove("modal-overlay-show");
+})
+$closeMap2.addEventListener("click", function(event){
+  event.preventDefault();
+  $map.classList.remove("modal-content-map-show");
+  $overlay.classList.remove("modal-overlay-show");
+})
+
+//Close Popup and Map Esk key
+window.addEventListener("keydown", function(event){
+    if (event.keyCode === 27) {
+        if ($popup.classList.contains("modal-content-show")) {
+            $popup.classList.remove("modal-content-show");
+            $overlay.classList.remove("modal-overlay-show");
+        }
+    }
+});
+window.addEventListener("keydown", function(event){
+    if (event.keyCode === 27) {
+        if ($map.classList.contains("modal-content-map-show")) {
+            $map.classList.remove("modal-content-map-show");
+            $overlay.classList.remove("modal-overlay-show");
+        }
+    }
+});
---
+++ projects/designstudios/js/popup.js
@@ -0,0 +1,56 @@
+var link = document.querySelector(".contact-us");
+var popup = document.querySelector(".contact-us-optional");
+var close = document.querySelector(".contact-us-optional-close-btn");
+var nameFocus = popup.querySelector("#name");
+var email = popup.querySelector("#email");
+var phone = popup.querySelector("#phone");
+var form = popup.querySelector("form");
+var storage = localStorage.getItem("name");
+
+//Form open, animation open
+link.addEventListener("click", function(event){         //отслеживает события клика
+    event.preventDefault();                             //ссылка не будет работать <a href=''> /#
+    popup.classList.add("contact-us-optional-show");    // добовляем клас css к тегу с класом .modal-content ($), без точки
+    
+    if (storage) {
+        nameFocus.value = storage;
+        email.focus();
+    } else {
+        nameFocus.focus();
+    }
+});
+
+//Close popup
+close.addEventListener("click", function(event){
+    event.preventDefault();
+    popup.classList.remove("contact-us-optional-show"); //delete class in tag
+    popup.classList.remove("modal-error");              //delete class in tag
+});
+
+//Error form data
+form.addEventListener("submit", function(event){
+    if (!nameFocus.value || !email.value || !phone.value) {
+         event.preventDefault();
+         popup.classList.add("modal-error");
+         
+    } else {
+        event.preventDefault();
+        localStorage.setItem("name", nameFocus.value);
+
+        console.log(nameFocus.value);
+        console.log(email.value);
+        console.log(phone.value);
+        console.log("All right");
+    }
+    
+});
+
+//Close popup key Esc
+window.addEventListener("keydown", function(event){
+    if (event.keyCode === 27) {
+        if (popup.classList.contains("contact-us-optional-show")) {
+	        popup.classList.remove("contact-us-optional-show");
+            popup.classList.remove("modal-error");
+        }
+    }
+});
